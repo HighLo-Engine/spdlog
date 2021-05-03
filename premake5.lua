40ba91ea4b2a226d813bd5ced3524e9a45fddef7
Cankar001 <info.can-karka@web.de>
2021-05-03 17:07:18 +0200

    Added premake script

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "spdlog"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+		{
+		"src/**.h"
+		}
+
+	includedirs
+		{
+		"include"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
